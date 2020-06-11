4e4bf807f38c515319827a4c3411b794b593a13b
Uranel <33304926+Uranel@users.noreply.github.com>
2020-06-11 14:12:48 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Test from "./components/Test/Test";
 import Intro from "./components/Test/intro";
 import Map from "./components/Test/googleMap";
 
-
 function App() {
   // return <Intro />;
   // return <GoogleMap />;
