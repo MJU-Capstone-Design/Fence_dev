6f0717fdd03d24fd4382012601efd9ea3e6a99c3
Uranel <33304926+Uranel@users.noreply.github.com>
2020-06-11 14:08:10 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import Test from "./components/Test/Test";
 import Intro from "./components/Test/intro";
 import Map from "./components/Test/googleMap";
-import Menu from "./components/Test/menu";
+
 
 function App() {
   // return <Intro />;
   // return <GoogleMap />;
-  // return <Menu />;
   return <Map />;
 }
 
