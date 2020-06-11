4574da1451467aa7acea9563e59e8cb0cff2d1e5
Uranel <33304926+Uranel@users.noreply.github.com>
2020-06-11 14:05:45 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Test from "./components/Test/Test";
 import Intro from "./components/Test/intro";
+import Map from "./components/Test/googleMap";
 import Menu from "./components/Test/menu";
 
 function App() {
-  return <Menu />;
+  // return <Intro />;
+  // return <GoogleMap />;
+  // return <Menu />;
+  return <Map />;
 }
 
 // class App extends Component {
